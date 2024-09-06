d316dcfbfce33b9a4aa5babad3986863aeb0d25e
Piyush Bagad <piyushnbagad11@gmail.com>
2024-09-06 07:42:45 +0100

    Adds result for InternVideo2 on EPIC

--- InternVideo2/api/timebound_eval_epic.py
+++ InternVideo2/api/timebound_eval_epic.py
@@ -78,6 +78,10 @@ def group_check(sim):
     return video_check(sim) and text_check(sim)
 
 
+def get_scores(sim):
+    return dict(video=video_check(sim), text=text_check(sim), group=group_check(sim))
+
+
 def get_check_for_row(row):
     id_a = row["id_a"]
     video_id_a = "_".join(id_a.split("_")[:-1])
@@ -180,39 +184,72 @@ if __name__ == "__main__":
         ckpt_name = "InternVideo2-stage2_1b-224p-f4.pt"
         model, _, config = load_model(ckpt_name=ckpt_name)
 
+    video_features_path = ".internvideo2_epic_video_features.pt"
+    text_features_path = ".internvideo2_epic_text_features.pt"
+    if os.path.exists(video_features_path):
+        video_features = torch.load(video_features_path)
+        text_features = torch.load(text_features_path)
+    else:
 
-    # Step 1: Compute video features for all IDs
-    ids_a = df_pair.id_a.unique()
-    ids_b = df_pair.id_b.unique()
-    ids = np.unique(np.concatenate([ids_a, ids_b]))
+        # Step 1: Compute video features for all IDs
+        ids_a = df_pair.id_a.unique()
+        ids_b = df_pair.id_b.unique()
+        ids = np.unique(np.concatenate([ids_a, ids_b]))
 
-    iterator = su.log.tqdm_iterator(ids, desc="Computing features")
-    video_features = dict()
-    text_features = dict()
-    for _id in iterator:
-        video_id = "_".join(_id.split("_")[:-1])
-        video_path = epic.get_video_path_basic(video_id)
-        assert os.path.exists(video_path)
-        row = df_main[df_main.narration_id == str(_id)].iloc[0].to_dict()
-        label = f"{row['verb']} {row['noun']}"
-        st = row["start_sec"]
-        et = row["stop_sec"]
-        try:
-            frames = load_frames_decord(video_path, st, et)
-        except:
-            print("Error in loading frames for", _id)
+        iterator = su.log.tqdm_iterator(ids, desc="Computing features")
+        video_features = dict()
+        text_features = dict()
+        for _id in iterator:
+            video_id = "_".join(_id.split("_")[:-1])
+            video_path = epic.get_video_path_basic(video_id)
+            assert os.path.exists(video_path)
+            row = df_main[df_main.narration_id == str(_id)].iloc[0].to_dict()
+            label = f"{row['verb']} {row['noun']}"
+            st = row["start_sec"]
+            et = row["stop_sec"]
+            try:
+                frames = load_frames_decord(video_path, st, et)
+            except:
+                print("Error in loading frames for", _id)
+                continue
+            with torch.no_grad():
+                video_feats = get_video_features(frames, model, config)
+                text_feats = get_text_features([label], model)
+            video_features[_id] = video_feats[0].cpu().numpy()
+            text_features[_id] = text_feats[0].cpu().numpy()
+        
+        # Save video features
+        torch.save(video_features, video_features_path)
+        torch.save(text_features, text_features_path)
+
+    N = len(df_pair)
+    results = []
+    iterator = su.log.tqdm_iterator(range(N), desc="Evaluating")
+    for i in iterator:
+        row = df_pair.iloc[i].to_dict()
+        id_a = row["id_a"]
+        id_b = row["id_b"]
+        if id_a not in video_features or id_b not in video_features:
             continue
-        with torch.no_grad():
-            video_feats = get_video_features(frames, model, config)
-            text_feats = get_text_features([label], model)
-        video_features[_id] = video_feats[0].cpu().numpy()
-        text_features[_id] = text_feats[0].cpu().numpy()
-    
-    # Save video features
-    torch.save(video_features, ".internvideo2_epic_video_features.pt")
-    torch.save(text_features, ".internvideo2_epic_text_features.pt")
-    import ipdb; ipdb.set_trace()
 
+        vid_a = video_features[id_a]
+        vid_b = video_features[id_b]
+        txt_a = text_features[id_a]
+        txt_b = text_features[id_b]
+        vid = np.stack([vid_a, vid_b])
+        txt = np.stack([txt_a, txt_b])
+
+        sim = (vid @ txt.T)
+        r = get_scores(sim)
+        results.append(r)
+    results = pd.DataFrame(results)
+    print(results.mean())
+
+    # Save results
+    os.makedirs("results", exist_ok=True)
+    results.to_csv("results/scores_epic_internvideo2-s2.csv", index=False)
+        
+    """
     # Evaluate on a single row
     debug = True
     if debug:
@@ -236,3 +273,4 @@ if __name__ == "__main__":
     # Save results
     os.makedirs("results", exist_ok=True)
     scores.to_csv("results/scores_epic_internvideo2-s2.csv", index=False)
+    """
